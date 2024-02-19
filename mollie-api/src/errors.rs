c7c05cba905d6196064ccfed8125154680cf43cd
Tjardo <info@tjardo.com>
2024-02-19 20:27:00 +0100

    WIP

--- mollie-api/src/errors.rs
+++ mollie-api/src/errors.rs
@@ -1,9 +1,32 @@
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
+
+use reqwest::Url;
 
 #[derive(thiserror::Error, Debug)]
 pub enum ApiError {
-    #[error("Request error")]
-    RequestError(#[from] reqwest::Error),
-    #[error("Parse error")]
+    EndpointError {
+        url: Url,
+        status: reqwest::StatusCode,
+    },
     ParseError(#[from] serde_json::Error),
+}
+
+impl From<reqwest::Error> for ApiError {
+    fn from(error: reqwest::Error) -> Self {
+        ApiError::EndpointError {
+            url: error.url().unwrap().clone(),
+            status: error.status().unwrap(),
+        }
+    }
+}
+
+impl Display for ApiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ApiError::EndpointError { url, status } => {
+                write!(f, "Endpoint error: {} - {}", url, status)
+            }
+            ApiError::ParseError(error) => write!(f, "Parse error: {}", error),
+        }
+    }
 }
--- src/errors.rs
+++ src/errors.rs
@@ -5,24 +5,26 @@ use mollie_api::errors::ApiError;
 
 #[derive(Debug)]
 pub enum CliError {
+    BadRequest(String),
     NotFound(String),
-    ParseError,
-    RequestError(String),
+    UnprocessableEntity(String),
     ServerError(String),
+    TooManyRequests(String),
+    ParseError,
 }
 
 impl From<ApiError> for CliError {
     fn from(error: ApiError) -> Self {
         match error {
-            ApiError::ParseError(_) => CliError::ParseError,
-            ApiError::RequestError(request) => match request.status() {
-                Some(status) => match status.as_u16() {
-                    404 => CliError::NotFound(request.url().unwrap().to_string()),
-                    500 => CliError::ServerError(request.url().unwrap().to_string()),
-                    _ => CliError::RequestError(request.url().unwrap().to_string()),
-                },
-                None => CliError::RequestError(request.url().unwrap().to_string()),
+            ApiError::EndpointError { url, status } => match status.as_u16() {
+                400 => CliError::BadRequest(url.to_string()),
+                404 => CliError::NotFound(url.to_string()),
+                422 => CliError::UnprocessableEntity(url.to_string()),
+                429 => CliError::TooManyRequests(url.to_string()),
+                500..=599 => CliError::ServerError(url.to_string()),
+                _ => CliError::ServerError(url.to_string()),
             },
+            ApiError::ParseError(_) => CliError::ParseError,
         }
     }
 }
@@ -30,14 +32,16 @@ impl From<ApiError> for CliError {
 impl Display for CliError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            CliError::NotFound(url) => write!(f, "Not found: {}", url.to_string().bold().red()),
+            CliError::BadRequest(url) => write!(f, "400 - Bad Request {}", url.bold().red()),
+            CliError::NotFound(url) => write!(f, "404 - Not Found: {}", url.bold().red()),
+            CliError::UnprocessableEntity(url) => {
+                write!(f, "422 - Unprocessable Entity: {}", url.bold().red())
+            }
+            CliError::TooManyRequests(url) => {
+                write!(f, "429 - Too Many Requests: {}", url.bold().red())
+            }
+            CliError::ServerError(url) => write!(f, "500 - Server Error: {}", url.bold().red()),
             CliError::ParseError => write!(f, "Parse error"),
-            CliError::RequestError(url) => {
-                write!(f, "Request error: {}", url.to_string().bold().red())
-            }
-            CliError::ServerError(url) => {
-                write!(f, "Server error: {}", url.to_string().bold().red())
-            }
         }
     }
 }
